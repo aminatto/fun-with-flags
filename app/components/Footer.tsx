2301e80e8ff9bf1c42815809dac8db2b3c168b83
aminatto <minattoamanda@gmail.com>
2025-01-16 18:41:29 -0300

    add footer and header components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <header className="py-6 mb-8">
+            <p className="text-center">
+                Made with ♥ by Amanda Minato
+            </p>
+        </header>
+    );
+}
+
+export default Footer;
