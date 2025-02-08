ca6d776a85794cae88117c82b0aea56e8991027b
aminatto <minattoamanda@gmail.com>
2025-02-08 19:21:57 -0300

    add links and border styles

--- app/country/[id]/page.tsx
+++ app/country/[id]/page.tsx
@@ -81,7 +81,7 @@ export default function Country() {
         .map(({ name, symbol }) => `${name} (${symbol})`)
         .join(", ");
     const topLevelDomain = tld ?? [];
-    const bordersIds = borders?.join(", ") ?? "";
+    const bordersIds = borders ?? [];
 
     return (
         <>
@@ -104,34 +104,36 @@ export default function Country() {
                 <div className="flex flex-col justify-center p-6 text-sm text-gray-600">
                     <h2 className="text-xl font-semibold mb-4">{countryName} ({id})</h2>
                     <div className="space-y-2">
-                        <div className="flex items-center gap-1">
-                            <span className="font-semibold">Capital:</span>
-                            <span>{capitalName}</span>
+                        <div>
+                            <span className="font-semibold">Capital:</span> {capitalName}
                         </div>
-                        <div className="flex items-center gap-1">
-                            <span className="font-semibold">Region:</span>
-                            <span>{region}</span>
+                        <div>
+                            <span className="font-semibold">Region:</span> {region}
                         </div>
-                        <div className="flex items-center gap-1">
-                            <span className="font-semibold">Population:</span>
-                            <span>{population}</span>
+                        <div>
+                            <span className="font-semibold">Population:</span> {population}
                         </div>
-                        <div className="flex items-center gap-1">
-                            <span className="font-semibold">Languages:</span>
-                            <span>{languagesNames}</span>
+                        <div>
+                            <span className="font-semibold">Languages:</span> {languagesNames}
                         </div>
-                        <div className="flex items-center gap-1">
-                            <span className="font-semibold">Currencies:</span>
-                            <span>{currenciesNames}</span>
+                        <div>
+                            <span className="font-semibold">Currencies:</span> {currenciesNames}
                         </div>
-                        <div className="flex items-center gap-1">
-                            <span className="font-semibold">Top Leven Domain:</span>
-                            <span>{topLevelDomain}</span>
+                        <div>
+                            <span className="font-semibold">Top Leven Domain:</span> {topLevelDomain}
                         </div>
-                        <div className="flex items-center gap-1">
-                            <span className="font-semibold">Borders:</span>
-                            <span>{bordersIds}</span>
-                        </div>
+                        <div className="md:max-w-80">
+                            <span className="font-semibold">Borders: </span>
+                            {bordersIds.length > 0
+                                ? bordersIds.map((borderId) => (
+                                    <Link key={borderId} href={`/country/${borderId}`}>
+                                        <button className="bg-gray-200 hover:bg-gray-300 text-xs mb-[6px] mr-[6px] px-[6px] py-[1.5px] rounded">
+                                            {borderId}
+                                        </button>
+                                    </Link>
+                                ))
+                                : "None"}
+                        </div>  
                     </div>
                 </div>
             </div>
